3277554e14223316de3c919fbb3c43a53fe10f1a
n4beel <nabeel3353045805@hotmail.com>
2023-03-19 06:26:50 +0500

    notifications complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,18 +15,26 @@ export class AppComponent {
         appId: "d0892710-d4bc-4d4e-8040-6dcaf444d79e",
         safari_web_id:
           "web.onesignal.auto.002ea938-3ebd-4740-ada1-6c17c5eb4600",
+        autoResubscribe: true,
       })
       .then(() => {
         console.log("running");
       });
+
+    this.oneSignal.on("notificationDisplay", (event) => {
+      console.log("OneSignal notification received:", event);
+      // Handle the event here, e.g., update the UI or store data
+    });
   }
 
   subscribeToNotifications(): void {
+    console.log("asking for permission");
     this.oneSignal.isPushNotificationsEnabled(async (enabled) => {
       console.log("is push notification enabled", enabled);
 
       if (enabled) {
         this.oneSignal.getUserId().then((userId: string) => {
+          // userId to be sent as FCM token
           console.log("User ID:", userId);
         });
       } else {
